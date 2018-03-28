0cb3b6d29562030dc6db123c88e32a1f267d527c
Jimmy Moon <ragingwind@gmail.com>
2018-03-28 20:21:17 +0900

    03 - apply style on page component

--- components/page.js
+++ components/page.js
@@ -3,6 +3,16 @@ import Link from 'next/link'
 const Menu = ({href, label}) => (
   <span>
     <Link href={href}><a>{label}</a></Link>
+    <style jsx>{`
+			{
+				padding: 6px 4px;
+			}
+			a {
+				color: white;
+				text-decoration: none;
+				font-size: 16px;
+			}
+		`}</style>
   </span>
 )
 
@@ -11,7 +21,13 @@ export default ({children}) => (
     <nav>
       <Menu href={'/'} label={'▲'} />
       {
-        ['news', 'newest', 'ask', 'jobs'].map(m => {
+        [
+          'news',
+          'newest',
+          'ask',
+          'show',
+          'jobs'
+        ].map(m => {
           return <Menu
             key={m}
             href={`/{m}`}
@@ -23,5 +39,37 @@ export default ({children}) => (
     <div>
       {children}
     </div>
+    <style jsx>{`
+			div {
+				width: 100%;
+				color: #333;
+				padding-top: 50px;
+			};
+		`}</style>
+    <style global jsx>{`
+      body {
+				font-family: Helvetica, sans-serif;
+			};
+			@media (max-width: 600px) {
+				header {
+					justify-content: none;
+				}
+			};
+			nav {
+				text-align: left;
+				background-color: black;
+				color: white;
+				padding: 12px;
+				position: fixed;
+				z-index: 1000;
+				top: 0;
+				left: 0;
+				right: 0;
+			};
+      a {
+				color: #0e0e0e;
+				text-decoration: none;
+			};
+    `}</style>
   </main>
 )
