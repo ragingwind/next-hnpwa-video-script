39b30be280559ce2f62d98b2542ec075b08ee271
Jimmy Moon <ragingwind@gmail.com>
2018-03-29 21:19:47 +0900

    07 - add more pages

---
+++ pages/_feed.js
@@ -0,0 +1,25 @@
+import {Component} from 'react'
+import Page from '../components/page'
+import Feeds from '../components/feeds'
+import fetch from '../lib/fetch'
+
+export default class extends Component {
+  static async getInitialProps({pathname, query}) {
+    const feed = pathname === '/' ? '/news' : pathname
+    const page = query.page ? `/${query.page}` : '1'
+
+    return {
+      feeds: await fetch(`${feed}/${page}`)
+    }
+  }
+  render () {
+    return (
+      <Page>
+        <Feeds
+          feeds={this.props.feeds}
+          url={this.props.url}
+        />
+      </Page>
+    )
+  }
+}
